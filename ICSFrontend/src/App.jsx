4aff31bcca10e94fde17b55b8c41725af403e265
Nevina Bradley <nevinabradley@gmail.com>
2024-04-05 18:34:59 -0400

    Added firebase

--- ICSFrontend/src/App.jsx
+++ ICSFrontend/src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import Login from './containers/Login'
 import CreateAccount from './containers/CreateAccount'
 
 function App() {
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <>
       <Router>
         <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={isLoggedIn ? <Navigate to="/home" /> : <Login setIsLoggedIn={setIsLoggedIn} />} />
+            <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
             <Route path="/create-account" element={<CreateAccount />} />
         </Routes>
       </Router>
